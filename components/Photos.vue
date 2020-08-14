80a2b0606c95138bf8c5ad8ae828ffc3be6169ae
Atakan <atakan@Atakans-MacBook-Air.local>
2020-08-14 18:55:04 +0300

    Added Photos component

---
+++ components/Photos.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="static grid grid-cols-5 grid-rows-5 gap-5 row-gap-5 overflow-auto">
+    <img v-bind:key="photo.id" v-for="photo in this.currentPhotos" :src="photo.img_src" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentPage: 0,
+      photosPerPage: 25,
+      currentPhotos: [],
+    }
+  },
+  created() {
+    this.getCurrentPhotos()
+  },
+  props: {
+    photos: Array,
+  },
+  methods: {
+    // getCurrentPhotos() {
+    //   const lastIndex = this.currentPage * this.photosPerPage
+    //   const firstIndex = lastIndex - this.photosPerPage
+    //   this.currentPhotos = this.$props.photos.slice(firstIndex, lastIndex)
+    //   console.log(this.currentPhotos)
+    // },
+  },
+}
+</script>
+
+<style>
+</style>
--- components/SearchForm.vue
+++ components/SearchForm.vue
@@ -1,37 +1,46 @@
 <template>
   <div>
-    <div class="flex flex-col w-screen items-center">
-      <form v-on:submit.prevent="getPhotos" class="search-form lg:w-1/2 w-screen space-y-4">
-        <label for="rover" class="form-label">Rover</label>
-        <select
-          name="rover"
-          id="rover"
-          class="rover-select w-1/2 focus:outline-none focus:border-orange-500"
-          v-model="rover"
-        >
-          <option value="curiosity">Curiosity</option>
-          <option value="opportunity">Opportunity</option>
-          <option value="spirit">Spirit</option>
-        </select>
-        <label for="sol" class="form-label">Sol</label>
-        <input
-          type="text"
-          id="sol"
-          class="sol-input placeholder-gray-900 focus:outline-none focus:border-orange-700 w-1/2"
-          placeholder="Mars Sol"
-          v-model="sol"
-        />
-        <button type="submit" class="btn w-1/2">Search</button>
-      </form>
+    <div v-if="this.photos.length === 0">
+      <div class="flex flex-col w-screen items-center">
+        <form v-on:submit.prevent="getPhotos" class="search-form lg:w-1/2 w-screen space-y-4">
+          <label for="rover" class="form-label">Rover</label>
+          <select
+            name="rover"
+            id="rover"
+            class="rover-select w-1/2 focus:outline-none focus:border-orange-500"
+            v-model="rover"
+          >
+            <option value="curiosity">Curiosity</option>
+            <option value="opportunity">Opportunity</option>
+            <option value="spirit">Spirit</option>
+          </select>
+          <label for="sol" class="form-label">Sol</label>
+          <input
+            type="text"
+            id="sol"
+            class="sol-input placeholder-gray-900 focus:outline-none focus:border-orange-700 w-1/2"
+            placeholder="Mars Sol"
+            v-model="sol"
+          />
+          <button type="submit" class="btn w-1/2">Search</button>
+        </form>
+      </div>
+    </div>
+    <div v-else>
+      <Photos v-bind:photos="this.photos" />
     </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import Photos from './Photos'
 
 export default {
   name: 'SearchForm',
+  components: {
+    Photos,
+  },
   data() {
     return {
       rover: '',
@@ -49,22 +58,10 @@ export default {
         const res = await axios.get(
           `https://mars-photos.herokuapp.com/api/v1/rovers/${this.rover}/photos?sol=${this.sol}`
         )
-        console.log(this.currentPage)
-
-        this.photos = res.data
-        this.loading = false
+        this.photos = res.data.photos
       } catch (err) {
         console.log(err)
       }
-    },
-    getCurrentPhotos() {
-      // Getting current photos
-      const lastIndex = this.currentPage * this.photosPerPage
-      const firstIndex = lastIndex - this.photosPerPage
-      const currentPhotos = this.photos.slice(firstIndex, lastIndex)
-      console.log(lastIndex)
-      console.log(firstIndex)
-      console.log(currentPhotos)
     },
   },
 }
