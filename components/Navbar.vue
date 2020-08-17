10939ac360388000b31662c2bbf9c5f214551d68
Atakan <atakan@Atakans-MacBook-Air.local>
2020-08-17 13:16:47 +0300

    Fixed Navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,12 +1,14 @@
 <template>
-  <nav class="navbar divide-x divide-orange-800">
-    <div class="links">
-      <nuxt-link to="/" class="homelink">Marspecto</nuxt-link>
-    </div>
-    <div class="inline-flex flex-grow">
-      <nuxt-link to="/rovers" class="link">Rovers</nuxt-link>
-      <nuxt-link to="/search" class="link">Search</nuxt-link>
-      <nuxt-link to="/about" class="link">About</nuxt-link>
+  <nav class="navbar">
+    <div class="links divide-x divide-orange-600 space-x-2 lg:space-x-4">
+      <div>
+        <nuxt-link to="/" class="homelink">Marspecto</nuxt-link>
+      </div>
+      <div class="inline-flex flex-grow">
+        <nuxt-link to="/rovers" class="link lg:text-xl">Rovers</nuxt-link>
+        <nuxt-link to="/search" class="link lg:text-xl">Search</nuxt-link>
+        <nuxt-link to="/about" class="link lg:text-xl">About</nuxt-link>
+      </div>
     </div>
   </nav>
 </template>
@@ -22,15 +24,15 @@ export default {
   @apply underline;
 }
 .navbar {
-  @apply flex relative w-full bg-black bg-opacity-75 justify-between p-3 items-center;
+  @apply flex  w-screen bg-black bg-opacity-75 justify-between p-4 items-center;
 }
 .links {
-  @apply items-center flex-shrink-0 text-white mr-6;
+  @apply flex items-center flex-shrink-0 text-white;
 }
 .homelink {
   @apply font-semibold text-2xl text-orange-600 tracking-widest underline;
 }
 .link {
-  @apply flex p-2 m-2 text-xl text-white;
+  @apply flex pl-2 text-lg text-white;
 }
 </style>
